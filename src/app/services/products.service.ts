d84a0c63749e2500a7e176ce45ed6612e70c323b
tounssi92 <59847686+tounssi92@users.noreply.github.com>
2021-04-14 15:17:19 +0200

    first

---
+++ src/app/services/products.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  constructor(private http: HttpClient) {
+    this.getJSON().subscribe(data => {
+      console.log(data);
+    });
+  }
+
+
+  public getJSON(): Observable<any> {
+    return this.http.get("./assets/data/products.json");
+  }
+}
